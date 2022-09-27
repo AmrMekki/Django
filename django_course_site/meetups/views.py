d64a5e83c84306538e8fae8c976f9a0ef2028268
Amr Mekki <amr.mekki21@gmail.com>
2022-09-27 23:01:11 +0200

    9

--- django_course_site/meetups/views.py
+++ django_course_site/meetups/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(request):
     meetups = [
-        {'title': 'A first meetup'},
-        {'title': 'A second meetup'}
+        {
+        'title': 'A first meetup', 
+        'location': 'New York', 
+        'slug': 'a-first-meetup'
+        },
+        {
+        'title': 'A second meetup', 
+        'location': 'Paris', 
+        'slug': 'a-second-meetup'
+        }
     ]
     return render(request, 'meetups/index.html', {
         'show_meetups': True,
